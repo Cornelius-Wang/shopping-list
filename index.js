3263a15976be23646a8323a735f6d5712ce9b42a
Cory Wang <cory.wang11@gmail.com>
2020-07-08 17:58:30 -0700

    lines 11-27 spaced out lines and sperated with quotations and addition signs because VSC was freaking out when I tried to use just one quotation around the whole block

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ $(function addOrRemoveItem() {
     const newItem = $(event.currentTarget).find('input[name="shopping-list-entry"]').val();
     
     /* Adding the new string + button code to the shopping list */
-    $('ul').append('<li><span class="shopping-item">' + newItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    $('ul').append(
+        
+        '<li>' +
+            '<span class="shopping-item">' + 
+            newItem + 
+            '</span>' +
+                '<div class="shopping-item-controls">' +
+                    '<button class="shopping-item-toggle">' +
+                        '<span class="button-label">check</span>' +
+                    '</button>' +
+                    '<button class="shopping-item-delete">' +
+                        '<span class="button-label">delete</span>' +
+                    '</button>' +
+                '</div>' +
+        '</li>'
+        
+    );
 
     });
 
